59e8c76ad6f994e1eb3137eec9e73b9832495fd3
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-13 14:48:28 +0900

    registered created models

--- social_blog/groups/admin.py
+++ social_blog/groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Group, GroupMember
+
+
+class GroupMemberInline(admin.TabularInline):
+  model = GroupMember
+
 
 # Register your models here.
+admin.site.register(Group)
