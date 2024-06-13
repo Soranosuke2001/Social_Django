a687a1fa85d7211a0477fe671c4b40c650293493
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-13 14:15:45 +0900

    created view classes create group: view to create a group detail group: view the details of a group list groups: view the list of all available groups

--- social_blog/groups/views.py
+++ social_blog/groups/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin 
+from django.views.generic import CreateView, DetailView, ListView
+
+from .models import *
+
 
 # Create your views here.
+class CreateGroup(LoginRequiredMixin, CreateView):
+  fields = ('name', 'description')
+  model = Group
+
+
+class SingleGroup(DetailView):
+  model = Group
+
+
+class ListGroups(ListView):
+  model = Group
+
