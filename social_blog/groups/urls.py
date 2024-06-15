ab3bdd5e4ed54b060c5cccf0fac0ede76ee54993
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-15 12:01:25 +0900

    urls to join and leave groups

--- social_blog/groups/urls.py
+++ social_blog/groups/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import *
+from .views import ListGroups, CreateGroup, SingleGroup, JoinGroup, LeaveGroup
 
 app_name = 'groups'
 
@@ -8,4 +8,7 @@ urlpatterns = [
   path("", ListGroups.as_view(), name="all"),
   path("new/", CreateGroup.as_view(), name="create"),
   path("posts/in/<slug:slug>/", SingleGroup.as_view(), name="single"),
+
+  path("join/<slug:slug>", JoinGroup.as_view(), name="join"),
+  path("leave/<slug:slug>", LeaveGroup.as_view(), name="leave"),
 ]
